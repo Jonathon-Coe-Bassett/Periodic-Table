59be8fed5d065d4b79818daf4716defcec9b7b02
Coe bassett <jonathon.bassett@mullenmail.com>
2017-03-10 09:28:47 -0700

    initoal

---
+++ Periodic-Table/src/Element.java
@@ -0,0 +1,6 @@
+
+public class Element
+	{
+		private String name;
+		private int charge;
+	}
---
+++ Periodic-Table/src/Runner.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Runner
+	{
+
+		public static void main(String[] argsv) throws IOException
+			{
+				// TODO Auto-generated method stub
+
+			}
+
+	}
