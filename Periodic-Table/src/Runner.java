b0c94d681a6744d17fca9e8fae8cb53057387bf6
michaelbrady6 <michael.brady108@gmail.com>
2017-03-16 10:27:22 -0600

    it no work

--- Periodic-Table/src/Runner.java
+++ Periodic-Table/src/Runner.java
@@ -1,12 +1,26 @@
 import java.io.*;
-
+import java.util.*;
 public class Runner
 	{
-
+		public static ArrayList<Element> elements = new ArrayList<Element>();
 		public static void main(String[] argsv) throws IOException
 			{
-				// TODO Auto-generated method stub
-
+				createTable();
+				for(Element x : elements)
+					{
+						System.out.println(x);
+					}
 			}
-
+		public static void createTable() throws FileNotFoundException
+		{
+			Scanner file = new Scanner(new File("periodicTable"));
+			while(file.hasNext())
+				{
+					String[] el = file.nextLine().split(",", 5);
+					elements.add(new Element(Integer.parseInt(el[0]), el[1], el[2], Double.parseDouble((el[3].replace("(.)", ""))), Integer.parseInt(el[4])));
+				}
+			
+			
+		}
+		
 	}
