c3dbba97ec27c8c4525ad950d7f78b375977e076
Coe bassett <jonathon.bassett@mullenmail.com>
2017-03-20 10:04:13 -0600

    Framework for CHemical

--- Periodic-Table/src/Chemical.java
+++ Periodic-Table/src/Chemical.java
@@ -2,30 +2,36 @@ import java.util.*;
 public class Chemical extends Element
 	{
 		private ArrayList<Element> contents=new ArrayList<>();
-		public Chemical(ArrayList<Element> e)
-		{
-			super(-1, name, name, atomicMass, atomicNumber);
-			this.contents=e;
-		}
+//		public Chemical(ArrayList<Element> e)
+//		{
+//			super(-1, name, name, atomicMass, atomicNumber);
+//			this.contents=e;
+//		}
 		public Chemical(Element e, int num)
 		{
+			super(-1, e.getSymbol()+ num, e.getName(), num * (e.getAtomicMass()), num*(((Chemical) e).calcCharge()));
 			for(int i=0; i<num; i++)
 			contents.add(e);
 		}
 		public Chemical(Chemical a, Chemical b)
 		{
+			super(-1, a.getSymbol()+b.getSymbol(), a.getName()+ " " + b.getName(), addMass(a, b), a.getCharge()+b.getCharge());
 			this.contents.add(a);
 			this.contents.add(b);
 			
 		}
-		public 
+		@Override
 		public String toString()
 		{
 			return null;
 			//for now
 		}
-		public int addMass()
+		public static double addMass(Element e, Element b)
 		{
-			
+			return e.getAtomicMass()+b.getAtomicMass(); 
+		}
+		public int calcCharge()
+		{
+			return 0;
 		}
 	}
